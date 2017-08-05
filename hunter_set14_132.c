4f5733fed308354e3b31f7afa4550e36f3b27e78
JokerBot <jbjaveed@gmail.com>
2017-08-06 05:27:31 +0530

    Update hunter_set14_132.c

--- hunter_set14_132.c
+++ hunter_set14_132.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+int main()
+{ int i,j,k,boun,n;
+    
+    
+    printf("enter the number: ");
+    scanf("%d",&n);
+    boun=2*n+1;
+    
+    for(j=n;j>0;j--)
+    {  
+        for(i=0;i<j;i++)
+        { printf("*");}
+        
+        for(k=1;k<=boun-2*j;k++)
+        { printf(" ");}
+        
+        for(i=0;i<j;i++)
+        { printf("*");}
+        
+    printf("\n");
+    }
+    
+    return 0;
+}
