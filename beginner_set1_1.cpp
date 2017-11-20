169e99776b48a4928cc8bf2e88d1ebc765f5a622
JokerBot <jbjaveed@gmail.com>
2017-11-20 21:39:47 +0530

    Create beginner_set1_1.cpp

---
+++ beginner_set1_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int  num;
+cin>>num;
+if(num<0){cout<<"negative";}
+else if(num>0){cout<<"positive";}
+else{cout<<"zero";}
+}
+
+/*
+
+input: 40
+output: positive
+
+input: -80
+output: negative
+
+input: 0
+output: zero
+
+*/
