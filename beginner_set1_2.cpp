8cac78679d1a07309b2173ddae4035ad13cf1677
JokerBot <jbjaveed@gmail.com>
2017-11-20 21:42:59 +0530

    Create beginner_set1_2.cpp

---
+++ beginner_set1_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    if(num%2==0)
+    {cout<<"even";}
+    else{cout<<"odd";}
+}
+
+/*
+input: 2
+output: even
+
+input:3
+output: odd
+*/
